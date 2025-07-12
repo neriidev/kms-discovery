376c9f25343ff58d230ec5e2469730c3a0ad82d4
neriidev <neriidev@gmail.com>
2025-07-12 11:36:45 -0300

    subindo sns

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ resource "aws_sns_topic" "meu_topico" {
 
 resource "aws_sns_topic_subscription" "minha_sub" {
   topic_arn = aws_sns_topic.meu_topico.arn
-  protocol  = "email"
+  protocol  = "http"
   endpoint  = "https://localhost.localstack.cloud:4566"  # ou "http" e coloque uma URL para testes locais
 }
