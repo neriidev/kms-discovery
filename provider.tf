f7d5d18b9ef704a094a1cd0d6124b4950e70046f
neriidev <neriidev@gmail.com>
2025-07-12 11:26:50 -0300

    subindo sns

--- provider.tf
+++ provider.tf
@@ -6,4 +6,12 @@ provider "aws" {
   endpoints {
     sns = "https://localhost.localstack.cloud:4566"
   }
+
+    # ESSENCIAIS PARA IGNORAR CHAMADAS REAIS
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_force_path_style         = true
+  insecure                    = true
+  
 }
